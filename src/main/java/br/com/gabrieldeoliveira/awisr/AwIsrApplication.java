62c59e336753ef9f4aef079c3605f30629c8ab04
Gabriel de Oliveira <br.gabrieldeoliveira@gmail.com>
2024-03-23 15:07:01 -0300

    Project created

---
+++ src/main/java/br/com/gabrieldeoliveira/awisr/AwIsrApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldeoliveira.awisr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwIsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwIsrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrieldeoliveira/awisr/AwIsrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldeoliveira.awisr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwIsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
